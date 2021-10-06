63f1decbb29a0f7869726a4e81e191fcb50dae1c
YaelSlushtz <yael32323@gmail.com>
2021-10-06 16:15:19 +0300

    hiii

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+hi tamar mamatzav?
 # dotNet5782_2395_0711
---
+++ Targil0/Program2395.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome2395();
+            welcome0711();
+            Console.ReadKey();
+        }
+
+        private static void welcome2395()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+        static partial void welcome0711();
+    }
+}
