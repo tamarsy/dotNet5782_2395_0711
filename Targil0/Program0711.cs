5db35695907586a4c157e249934b1192a175df94
tamarsy <tamar0556789257@gamil.com>
2021-10-06 16:09:00 +0300

    function Wellcome0711

---
+++ Targil0/Program0711.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Wellcome0711()
+        {
+            Console.WriteLine("i am also here yael");
+        }
+    }
+}
