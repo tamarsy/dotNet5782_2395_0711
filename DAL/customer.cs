e631107675941d74cb490a9d4554eae9933bd284
YaelSlushtz <yael32323@gmail.com>
2021-10-14 15:44:07 +0300

    new class and enums

---
+++ DAL/customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double longitude { get; set; }
+            public double lattitude { get; set; }
+            public override string ToString()
+            {
+                return "Parcel";
+            }
+
+            public override string ToString()
+            {
+                return "Customer";
+            }
+        }
+
+
+    }
+}
