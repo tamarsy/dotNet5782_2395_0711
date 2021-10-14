3a4ff0b937afce4c31c6a2143fe86b2d26583258
YaelSlushtz <yael32323@gmail.com>
2021-10-14 16:57:41 +0300

    jm

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL.DO;
 
-namespace IDAL
+
+namespace DalObject
 {
-    class DataSource
+    internal class DataSource
     {
+        static internal Drone[] DronesArr = new Drone[10];
+        static internal Station[] StationsArr = new Station[5];
+        static internal Customer[] CustomerArr = new Customer[100];
+        static internal Parcel[] ParcelArr = new Parcel[100];
+
         class Config
         {
 
         }
     }
 }
+
