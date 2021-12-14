5982516c71af5cf28dd7a4ef57a52d31a47c96cf
YaelSlushtz <yael32323@gmail.com>
2021-12-14 10:56:39 +0200

    change

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IBL;
 
 namespace WpfApp1
 {
@@ -23,6 +24,12 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            IBL.IBL bl = new IBL.BL();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           BL.
         }
     }
 }
